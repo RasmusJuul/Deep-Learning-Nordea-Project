f16eb4abbcf28ddcb99430d6df1add52550e8ffe
Rasmus <s164564@student.dtu.dk>
2019-11-17 21:36:19 +0100

    trying different thing

--- Scripts/get_data.py
+++ Scripts/get_data.py
@@ -4,17 +4,33 @@ Created on Wed Nov 13 15:29:44 2019
 
 @author: rasmu
 """
+import numpy as np
+try:
+    from virk import *
+    import liquer.ext.basic
+    import liquer.ext.lq_pandas
+    from liquer.cache import FileCache, set_cache
+    set_cache(FileCache("cache"))
+except:
+    print('already loaded')
 
-#from virk import *
-#import liquer.ext.basic
-#import liquer.ext.lq_pandas
-#from liquer.cache import FileCache, set_cache
-#set_cache(FileCache("cache"))
 
 register_df = register()
+idx = ['entity', 'GrossResult','GrossProfitLoss','ProfitLoss','Revenue','Assets']
+compInfo = np.empty((register_df.shape[0],len(idx)))
 
-temp = cvrdf(register_df['cvrNummer'].values[1])
-compInfo = temp.iloc[temp['GrossResult'].dropna().index]
+for (i,cvr_nr) in enumerate(register_df['cvrNummer']):
+    temp = cvrdf(cvr_nr)
+    for (j,entry) in enumerate(idx):
+        try:
+            temp2 = temp[entry]
+            compInfo[i,j] = np.mean(temp[entry].dropna().values.astype('float'))
+        except:
+            compInfo[i,j] = 0
 
-CoI = ['entity','GrossResult','ProfitLoss','ProfitLossFromOrdinaryOperatingActivities'] #'Revenue' '_score'
-temp2 = compInfo[CoI]
+# Attemt at getting data of one company from different years (not working)
+#cvr_nr = register_df['cvrNummer'][0]
+#date = register_df['regnskabsperiode_slutDato'][0].replace('-','')
+#date2 = str(int(date[:4])-1)+date[4:]
+#temp1 = cvrdf(register_df['cvrNummer'],day=date)
+#temp2 = cvrdf(register_df['cvrNummer'],day=date2)
