a89d7acf55d57768f4289997f89c1243d587ba0a
Rasmus <s164564@student.dtu.dk>
2019-11-24 12:50:40 +0100

    data and few changes to network

--- Scripts/get_data.py
+++ Scripts/get_data.py
@@ -1,30 +1,62 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 13 15:29:44 2019
+import virk
+from utils import daterange
+from datetime import timedelta, date
 
-@author: rasmu
-"""
-import numpy as np
+import liquer.ext.basic
+import liquer.ext.lq_pandas
+from liquer.cache import FileCache, set_cache
 
-try:
-    from virk import *
-    import liquer.ext.basic
-    import liquer.ext.lq_pandas
-    from liquer.cache import FileCache, set_cache
-    set_cache(FileCache("cache"))
-except:
-    print('already loaded')
+# temp imports
+import pprint
 
+data_save_path = "data/"
 
-register_df = register()
-idx = ['entity', 'GrossResult','GrossProfitLoss','ProfitLoss','Revenue','Assets']
-compInfo = np.empty((register_df.shape[0],len(idx)))
+# Keep cache in case of crashing while working on it!
+set_cache(FileCache("cache"))
 
-for (i,cvr_nr) in enumerate(register_df['cvrNummer']):
-    temp = cvrdf(cvr_nr)
-    for (j,entry) in enumerate(idx):
-        try:
-            temp2 = temp[entry]
-            compInfo[i,j] = np.mean(temp[entry].dropna().values.astype('float'))
-        except:
-            compInfo[i,j] = 0
+# Years interested in
+years = [2012,2013,2014,2015,2016,2017,2018,2019]
+
+for year in years:
+    # First get unique CVR numbers
+    # between start and end date:
+    # Format of date in date(year,month,day)
+    st_date = date(year, 1, 1)
+    en_date = date(year, 12, 30)
+
+    print(year)
+
+    # Set of all CVR numbers and dates we aquired them as a tuple
+    all_company_cvr = dict()
+
+    for single_date in daterange(st_date, en_date):
+        cur_date = single_date.strftime("%Y%m%d")
+
+        if cur_date[-2:] == "01":
+            print(cur_date)
+
+        register_df = virk.register(day=cur_date)
+        cvr_nums = register_df["cvrNummer"].unique()
+
+        for temp_cvr in cvr_nums:
+            if temp_cvr in all_company_cvr:
+                all_company_cvr[temp_cvr] += (cur_date,)
+            else:
+                all_company_cvr[temp_cvr] = (cur_date,)
+
+    with open(data_save_path+"cvr_to_dates_{0}.csv".format(year),"w") as outout:
+        outout.write("cvr,dates \n")
+        for company_cvr in all_company_cvr.keys():
+            outout.write(",".join([str(company_cvr)] + list(map(str,[*all_company_cvr[company_cvr]])))+"\n")
+
+    dist = dict()
+    for i in all_company_cvr:
+        num = len(all_company_cvr[i])
+        if num in dist:
+            dist[num] += 1
+        else:
+            dist[num] = 1
+
+    print("{0} year statistic".format(year))
+    pprint.pprint(dist)
+    print("Number of companies: ",len(all_company_cvr.keys()))
